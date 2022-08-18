1d95a8bf7959501507bb5efb1dabeb4021047a50
Sahakanush <95763931+Sahakanush@users.noreply.github.com>
2022-08-18 18:00:26 +0400

    Update change_tag.py

--- work_with_mf/change_tag.py
+++ work_with_mf/change_tag.py
@@ -3,7 +3,9 @@ import yaml
 with open('mf') as file:
   data_dict = yaml.load(file)
   print(data_dict)
-  data_dict['image'] = "hello world!!!!!!"
+  
+  data_dict['spec']['template']['spec']['containers']['- name']['image'] = "hello world!!!!!!"
   
   with open('mf', 'w') as f:
         yaml.dump(data_dict, f)
+    
