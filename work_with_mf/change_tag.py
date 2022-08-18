b979bbcc2bc3d113134280bb0677d0f78e63f604
Sahakanush <95763931+Sahakanush@users.noreply.github.com>
2022-08-18 19:39:21 +0400

    Update change_tag.py

--- work_with_mf/change_tag.py
+++ work_with_mf/change_tag.py
@@ -5,10 +5,9 @@ version = sys.argv[1]
 
 with open('../../manifest/mf') as file:
   data_dict = yaml.load(file)
-  print(data_dict)
   
   data_dict['spec']['template']['spec']['containers'][0]['image'] = version
   
-  with open('mf', 'w') as f:
+  with open('../../manifest/mf', 'w') as f:
         yaml.dump(data_dict, f)
     
