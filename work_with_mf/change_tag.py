bc325f781883097114cf756e72af78ebec74726b
Sahakanush <95763931+Sahakanush@users.noreply.github.com>
2022-08-18 18:30:25 +0400

    Update change_tag.py

--- work_with_mf/change_tag.py
+++ work_with_mf/change_tag.py
@@ -1,10 +1,13 @@
 import yaml
+import sys
+
+version = sys.argv[1]
 
 with open('mf') as file:
   data_dict = yaml.load(file)
   print(data_dict)
   
-  data_dict['spec']['template']['spec']['containers'][0]['image'] = "hello world!!!!!!"
+  data_dict['spec']['template']['spec']['containers'][0]['image'] = version
   
   with open('mf', 'w') as f:
         yaml.dump(data_dict, f)
