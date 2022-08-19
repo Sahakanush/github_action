27d96e3b5eb0f108fcd8def0ebe2627b098d7668
Sahakanush <95763931+Sahakanush@users.noreply.github.com>
2022-08-19 14:46:34 +0400

    Update change_tag.py

--- work_with_mf/change_tag.py
+++ work_with_mf/change_tag.py
@@ -3,11 +3,11 @@ import sys
 
 version = sys.argv[1]
 
-with open('../../../testingInfra/WebApps/WebApps/identidockui.yaml ') as file:
+with open('identidockui.yaml ') as file:
   data_dict = yaml.load(file)
   
   data_dict['spec']['template']['spec']['containers'][0]['image'] = version
   
-  with open('../../../testingInfra/WebApps/WebApps/identidockui.yaml', 'w') as f:
+  with open('identidockui.yaml', 'w') as f:
         yaml.dump(data_dict, f)
     
